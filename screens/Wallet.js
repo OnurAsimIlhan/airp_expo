82914b2afa53e5d403d7b0bc4f3a87836a054536
OnurAsimIlhan <63584455+OnurAsimIlhan@users.noreply.github.com>
2022-08-08 17:51:12 +0300

    wallet improved

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -2,6 +2,7 @@ import { View, Text, Image, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import CreditCards from '../components/CreditCards';
+import { PlusCircleIcon, UserAddIcon } from 'react-native-heroicons/outline';
 
 const Wallet = () => {
   const navigation = useNavigation();
@@ -18,15 +19,18 @@ const Wallet = () => {
   }, []);
 
   return (
-    <View>
+    <ScrollView>
       <View className='m-5 flex-row justify-between bg-[#72cfe7ff] rounded-xl'>
-        <Text className='my-8 mx-2'>Mevcut Bakiye: 52.45 ₺</Text>
-        <Image source={require('../assets/tlicon.png')}
+        <Image source={require('../assets/wallet.png')}
           className='h-16 w-16 mx-4 my-2'
+        />
+        <Text className='my-8 mx-2 font-bold'>Mevcut Bakiye: 52.45 ₺</Text>
+        <Image source={require('../assets/tlicon.png')}
+          className='h-16 w-16 mx-4 my-2 rounded-full'
         />
       </View>
 
-      <Text className='mx-7'>Kartlarım</Text>
+      <Text className='mx-7 font-bold'>Kartlarım</Text>
       <ScrollView
         horizontal
         contentContainer={{
@@ -35,15 +39,54 @@ const Wallet = () => {
         showsHorizontalScrollIndicator={false}
         className='pt-2 px-2'
       >
-        <CreditCards/>
-        <CreditCards/>
-        <CreditCards/>
-        <CreditCards/>
-        <CreditCards/>
-        <CreditCards/>
-        <CreditCards/>
+        <CreditCards />
+        <CreditCards />
+        <CreditCards />
+        <CreditCards />
+        <CreditCards />
+
       </ScrollView>
-    </View>
+      <View className='items-end mx-2'>
+        <PlusCircleIcon size={40} color={"black"} />
+      </View>
+
+
+      <Text className='font-bold mx-7'>Son İşlemler</Text>
+
+      <View className='mx-5 my-2 flex-row justify-between bg-[#72cfe7ff] rounded-xl'>
+        <Text className='font-bold mx-2 my-5'>Taksi        -52.45 ₺ </Text>
+        
+        <Text className='font-bold mx-2 my-5'>Detaylar</Text>
+      </View>
+
+      <View className='mx-5 my-2 flex-row justify-between bg-[#72cfe7ff] rounded-xl'>
+        <Text className='font-bold mx-2 my-5'>Taksi        -52.45 ₺ </Text>
+        
+        <Text className='font-bold mx-2 my-5'>Detaylar</Text>
+      </View>
+
+      <View className='mx-5 my-2 flex-row justify-between bg-[#72cfe7ff] rounded-xl'>
+        <Text className='font-bold mx-2 my-5'>Taksi        -52.45 ₺ </Text>
+        
+        <Text className='font-bold mx-2 my-5'>Detaylar</Text>
+      </View>
+
+      <View className='mx-5 my-2 flex-row justify-between bg-[#72cfe7ff] rounded-xl'>
+        <Text className='font-bold mx-2 my-5'>Taksi        -52.45 ₺ </Text>
+        
+        <Text className='font-bold mx-2 my-5'>Detaylar</Text>
+      </View>
+      
+      <View className='mx-5 my-2 flex-row justify-between bg-[#72cfe7ff] rounded-xl'>
+        <Text className='font-bold mx-2 my-5'>Taksi        -52.45 ₺ </Text>
+        
+        <Text className='font-bold mx-2 my-5'>Detaylar</Text>
+      </View>
+
+      
+      
+
+    </ScrollView>
   )
 }
 
